7451c2459abb670744475e09b578844986c997a4
liukai <2393677557@qq.com>
2020-09-03 08:27:54 +0800

    提交idea

---
+++ test-git2/src/com/jxd/test/test.java
@@ -0,0 +1,11 @@
+package com.jxd.test;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("你好世界");
+        System.out.println("你好世界");
+        System.out.println("你好世界");
+        System.out.println("你好世界");
+        System.out.println("你好世界");
+    }
+}
